e9e92fa99825bbde12357bce257d7e202cecd4fe
Leah Onutor <leah@onutor.de>
2018-10-29 16:24:58 +0100

    update

--- htdocs/include/header.php
+++ htdocs/include/header.php
@@ -2,25 +2,25 @@
   <head>
     <title>Meine Seite</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet"  type="text/css" href="css/style.scss">
+    <link rel="stylesheet"  type="text/css" href="css/style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="/images/logo.png">
   </head>
   
   <body>
-    <div class="container-fluid navig">
-      <nav class="navbar navbar-expand-lg justify-content-center navbar-light navFixed">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#">ok</a>
-            </li>
-          </ul>
+        <div class="collapse navbar-collapse justify-content-md-center" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link" href="#">Start</a>
+            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Link</a>
+          </div>
         </div>
       </nav>
     </div>
-    
     <div class="container">
 
