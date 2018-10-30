57311b14858baad81121c064a0019c577f9745a0
Leah Onutor <leah@onutor.de>
2018-10-30 17:19:41 +0100

    Design Änderung

--- htdocs/include/header.php
+++ htdocs/include/header.php
@@ -1,26 +1,28 @@
 <html>
   <head>
-    <title>Meine Seite</title>
+    <title>Leah Onutor</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet"  type="text/css" href="css/style.css">
     <link rel="shortcut icon" type="image/x-icon" href="/images/logo.png">
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
   </head>
   
   <body>
-    <div class="container-fluid">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light navigation">
+    <div class="header">
+      Leah Onutor
+    </div>
+      <nav class="navbar navbar-expand-lg navbar-light navigation">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-md-center" id="navbarNavAltMarkup">
           <div class="navbar-nav">
             <a class="nav-link" href="#">Start</a>
+<!--            <a class="nav-link" href="#">Link</a>
             <a class="nav-link" href="#">Link</a>
-            <a class="nav-link" href="#">Link</a>
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Link</a>-->
           </div>
         </div>
       </nav>
-    </div>
     <div class="container">
 
--- htdocs/index.php
+++ htdocs/index.php
@@ -3,7 +3,18 @@ $serverpath = $_SERVER['DOCUMENT_ROOT'];
 include $serverpath . '/include/header.php';
 ?>
 <div class="home">
-  <div class="title">WILLKOMMEN</div>
+  <div>
+  <p>W</p>
+  <p>I</p>
+  <p>L</p>
+  <p>L</p>
+  <p>K</p>
+  <p>O</p>
+  <p>M</p>
+  <p>M</p>
+  <p>E</p>
+  <p>N</p>
+</div>
   <div class="subtitle">bei Leah Onutor</div>
 </div>
 
